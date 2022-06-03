710171bf741db9e0d28cb8e6fbbfb5bae9ded960
swabbie89 <65784977+swabbie89@users.noreply.github.com>
2022-06-03 12:22:46 -0400

    Add files via upload
    Don't forget to add the scene to the build settings or it won't load.

---
+++ MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+    public GameObject optionsScreen, loadingScreen, loadingIcon;
+    public Text loadingText;
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadStart());
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public IEnumerator LoadStart()
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(firstLevel);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress >= .9f)
+            {
+                loadingText.text = "Press any key to continue";
+                loadingIcon.SetActive(false);
+
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+
+                    Time.timeScale = 1f;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
